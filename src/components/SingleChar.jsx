85e037bff54daafe9400c594b518875712d2559a
Karol Grydz <grydzkarol@gmail.com>
2020-07-17 13:02:48 +0200

    STYLE SINGLE

--- src/components/SingleChar.jsx
+++ src/components/SingleChar.jsx
@@ -1,66 +1,80 @@
 import React, { useEffect, useState } from 'react';
+import '../assets/sass/card.scss';
 import { oneCharacter } from '../helpers/bbApi';
-import { Container, Paper, Typography, makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles({
-  card: {
-    transition: 'transform 0.6s',
-    transformStyle: 'preserve-3d',
-    height: '300px',
-    '&:hover': {
-      transform: 'rotateY(180deg)',
-    },
-  },
-  cardBack: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    backfaceVisibility: 'hidden',
-    transform: 'rotateY(180deg)',
-  },
-  cardFront: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    backfaceVisibility: 'hidden',
-  },
-});
+import { Typography, Modal, Grid } from '@material-ui/core';
 
 export const SingleChar = ({ id }) => {
   const [char, updateChar] = useState({});
-  const classes = useStyles();
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     (async function () {
       if (true) {
         const oneChar = await oneCharacter(id);
-        console.log(oneChar[0]);
+        // console.log(oneChar[0]);
         updateChar(oneChar[0]);
       }
     })();
   }, []);
+
+  const handleOpen = () => {
+    setOpen(true);
+    console.log('open');
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    console.log('close');
+  };
+
   return (
-    <Container>
-      <Paper
-        variant='outlined'
-        elevation={3}
-        style={{ backgroundColor: '#003300', color: '#e0e0e0' }}
-        className={classes.card}
-      >
-        <div className={classes.cardFront}>
-          <img
-            src={char.img}
-            alt=''
-            style={{
-              height: 'auto',
-              maxWidth: '100%',
-              display: 'block',
-              margin: 'auto',
-            }}
-          />
+    <div className="card">
+      <div className="card-inner" onClick={handleOpen}>
+        <div className="card-front">
+          <img src={char.img} alt="character picture" />
         </div>
-        <div className={classes.cardBack}>{char.name}</div>
-        {/* <Typography variant="h5">My name is {char.name}</Typography>{' '} */}
-      </Paper>
-    </Container>
+        <div className="card-back">
+          <Typography variant="subtitle1">
+            <b>Name:</b> {char.name}
+          </Typography>{' '}
+          <Typography variant="subtitle1">
+            <b>Nickname:</b> {char.nickname}
+          </Typography>{' '}
+          <Typography variant="subtitle1">
+            <b>Birthday:</b> {char.birthday}
+          </Typography>{' '}
+          <Typography variant="subtitle1">
+            <b>Alive:</b> {char.status}
+          </Typography>{' '}
+          <Typography variant="subtitle1">
+            <i>Click for more informations</i>
+          </Typography>{' '}
+        </div>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#003300',
+        }}
+      >
+        <Grid item>
+          <Grid item md={6} sm={6} xs={12} style={{ textAlign: 'center' }}>
+            <img
+              src={char.img}
+              alt="character picture"
+              style={{ height: '200px', width: '200px' }}
+            />
+          </Grid>
+          <Grid item md={6} sm={6} xs={12}>
+            {char.name}
+          </Grid>
+        </Grid>
+      </Modal>
+    </div>
   );
 };
