873314c743fe71f21bf552df786a653fdb979d65
Karol Grydz <grydzkarol@gmail.com>
2020-07-24 18:01:12 +0200

    ITEMMODAL

--- src/components/ItemModal.jsx
+++ src/components/ItemModal.jsx
@@ -1,13 +1,77 @@
-import React, {useState} from 'react'
-import {  Modal } from '@material-ui/core';
+import React from 'react';
 
-export const ItemModal = ({}) => {
-  const [open, setOpen] = useState(false);
+export const ItemModal = ({
+  name,
+  nickname,
+  occupation,
+  better_call_saul_appearance,
+  appearance,
+  status,
+  birthday,
+  img,
+  death,
+  quotes,
+}) => {
   return (
-    <div>
-      <Modal>
-        
-      </Modal>
+    <div className='items'>
+      <div className='item'>
+        <img src={img} alt='character picture' />
+      </div>
+      <div className='item'>
+        <h2>{nickname}</h2>
+        <p>
+          <b>Name: </b>
+          {name}
+        </p>
+        <p>
+          <b>Nickname: </b>
+          {nickname}
+        </p>
+        <p>
+          <b>Birthday: </b>
+          {birthday}
+        </p>
+        <p>
+          <b>Alive: </b>
+          {status}
+        </p>
+        <p>
+          <b>Breaking Bad Seasons: </b>
+          {appearance && appearance.map((item) => <a key={item}> {item},</a>)}
+        </p>
+        <p>
+          <b>Better Call Saul Season: </b>
+          {better_call_saul_appearance ? (
+            better_call_saul_appearance.length != 0 ? (
+              better_call_saul_appearance.map((item) => (
+                <a key={item}> {item},</a>
+              ))
+            ) : (
+              <a>Not appear</a>
+            )
+          ) : (
+            <a>Not appear</a>
+          )}
+        </p>
+        <p>
+          <b>Occupation: </b>
+          {occupation.map((item) => (
+            <a key={item}> {item},</a>
+          ))}
+        </p>
+        <p>
+          <b>Number of people killed: </b>
+          {death}
+        </p>
+        <b>Best quotes: </b>
+        <ol>
+          {quotes.length ? (
+            quotes.map((char) => <li key={char.quote_id}>{char.quote}</li>)
+          ) : (
+            <p>None</p>
+          )}
+        </ol>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/SingleChar.jsx
+++ src/components/SingleChar.jsx
@@ -2,31 +2,31 @@ import React, { useEffect, useState } from 'react';
 import '../assets/sass/card.scss';
 import { deathCountByCharacter, qoutesByCharacter } from '../helpers/bbApi';
 import { Typography, Modal } from '@material-ui/core';
+import { ItemModal } from './ItemModal';
 
-export const SingleChar = ({
-  name,
-  nickname,
-  occupation,
-  better_call_saul_appearance,
-  appearance,
-  status,
-  birthday,
-  img,
-}) => {
+export const SingleChar = (props) => {
   const [death, updateDeath] = useState([]);
   const [quotes, updateQuotes] = useState([]);
   const [open, setOpen] = useState(false);
+  const {
+    name,
+    nickname,
+    occupation,
+    better_call_saul_appearance,
+    appearance,
+    status,
+    birthday,
+    img,
+  } = props;
   useEffect(() => {
     let isCanceled = false;
     (async function () {
       if (true) {
-        const strName = name.replace(' ', '+');
+        const strName = props.name.replace(' ', '+');
         const deathCounts = await deathCountByCharacter(strName);
         const bestQoutes = await qoutesByCharacter(strName);
-        if (isCanceled) {
-          updateDeath(deathCounts[0].deathCount);
-          updateQuotes(bestQoutes.slice(0, 9));
-        }
+        if (!isCanceled) updateDeath(deathCounts[0].deathCount);
+        if (!isCanceled) updateQuotes(bestQoutes.slice(0, 9));
       }
     })();
     return () => {
@@ -39,25 +39,25 @@ export const SingleChar = ({
   };
 
   return (
-    <div className="card">
-      <div className="card-inner" onClick={handleOpen}>
-        <div className="card-front">
-          <img src={img} alt="character picture" />
+    <div className='card'>
+      <div className='card-inner' onClick={handleOpen}>
+        <div className='card-front'>
+          <img src={props.img} alt='character picture' />
         </div>
-        <div className="card-back">
-          <Typography variant="subtitle1">
-            <b>Name:</b> {name}
+        <div className='card-back'>
+          <Typography variant='subtitle1'>
+            <b>Name:</b> {props.name}
           </Typography>{' '}
-          <Typography variant="subtitle1">
-            <b>Nickname:</b> {nickname}
+          <Typography variant='subtitle1'>
+            <b>Nickname:</b> {props.nickname}
           </Typography>{' '}
-          <Typography variant="subtitle1">
-            <b>Birthday:</b> {birthday}
+          <Typography variant='subtitle1'>
+            <b>Birthday:</b> {props.birthday}
           </Typography>{' '}
-          <Typography variant="subtitle1">
-            <b>Alive:</b> {status}
+          <Typography variant='subtitle1'>
+            <b>Alive:</b> {props.status}
           </Typography>{' '}
-          <Typography variant="subtitle1">
+          <Typography variant='subtitle1'>
             <i>Click for more informations</i>
           </Typography>{' '}
         </div>
@@ -65,71 +65,11 @@ export const SingleChar = ({
       <Modal
         open={open}
         onClose={handleOpen}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        className="modal"
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+        className='modal'
       >
-        <div className="items">
-          <div className="item">
-            <img src={img} alt="character picture" />
-          </div>
-          <div className="item">
-            <h2>{nickname}</h2>
-            <p>
-              <b>Name: </b>
-              {name}
-            </p>
-            <p>
-              <b>Nickname: </b>
-              {nickname}
-            </p>
-            <p>
-              <b>Birthday: </b>
-              {birthday}
-            </p>
-            <p>
-              <b>Alive: </b>
-              {status}
-            </p>
-            <p>
-              <b>Breaking Bad Seasons: </b>
-              {appearance &&
-                appearance.map((item) => <a key={item}> {item},</a>)}
-            </p>
-            <p>
-              <b>Better Call Saul Season: </b>
-              {better_call_saul_appearance ? (
-                better_call_saul_appearance.length != 0 ? (
-                  better_call_saul_appearance.map((item) => (
-                    <a key={item}> {item},</a>
-                  ))
-                ) : (
-                  <a>Not appear</a>
-                )
-              ) : (
-                <a>Not appear</a>
-              )}
-            </p>
-            <p>
-              <b>Occupation: </b>
-              {occupation.map((item) => (
-                <a key={item}> {item},</a>
-              ))}
-            </p>
-            <p>
-              <b>Number of people killed: </b>
-              {death}
-            </p>
-            <b>Best quotes: </b>
-            <ol>
-              {quotes.length ? (
-                quotes.map((char) => <li key={char.quote_id}>{char.quote}</li>)
-              ) : (
-                <p>None</p>
-              )}
-            </ol>
-          </div>
-        </div>
+        <ItemModal {...props} death={death} quotes={quotes} />
       </Modal>
     </div>
   );
